12a277dc0d493b8c48a66d4b62db0384a4979f95
Javier Castro <javier.alejandro.castro@gmail.com>
2021-04-27 19:02:59 -0300

    Actualizaciones npm

--- demo/client.jsx
+++ demo/client.jsx
@@ -1,5 +1,6 @@
 // @flow
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
